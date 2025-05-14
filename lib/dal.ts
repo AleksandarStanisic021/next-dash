256d5fa3e15aa648c95b275547022b524193c03b
ciandre <a.stanisic.codealkonost@gmail.com>
2025-05-14 18:54:19 +0200

    Refactor dashboard layout and page components; implement issue retrieval and display logic

--- lib/dal.ts
+++ lib/dal.ts
@@ -1,9 +1,7 @@
 import { db } from '@/db'
 import { getSession } from './auth'
 import { eq } from 'drizzle-orm'
-import { cache } from 'react'
 import { issues, users } from '@/db/schema'
-import { mockDelay } from './utils'
 
 // Current user
 export const getCurrentUser = async () => {
